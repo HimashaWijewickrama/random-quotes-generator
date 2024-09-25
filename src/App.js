1f87951d7e1d4a51534dbbc20210727460d657bd
myusername <himashawije5678@gmail.com>
2024-09-25 16:07:10 +0530

    completed-application

--- src/App.js
+++ src/App.js
@@ -1,47 +1,11 @@
 import "./App.css";
-import { Button, Card } from "react-bootstrap";
-import { BsChatSquareQuote } from "react-icons/bs";
-import { useState } from "react";
+import React from "react";
+import Home from "./Home";
 
 function App() {
-  const [bgColor, setBgColor] = useState("#642b73");
-
-  const handleChangeBgColor = (e) => {
-    setBgColor(bgColor === "#642b73" ? "#c6426e" : "#983770");
-  };
-  
   return (
     <div className="App">
-      <header className="App-header">
-        <Card style={{ backgroundColor: "#fff", color: bgColor }}>
-          <Card.Header>Quote For The Good Day</Card.Header>
-          <Card.Body>
-            <blockquote className="blockquote mb-0">
-              <p>
-                {" "}
-                <BsChatSquareQuote size={50} />
-                <br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                posuere erat a ante. <br />
-              </p>
-              <footer className="blockquote-footer">
-                Someone famous in <cite title="Source Title">Source Title</cite>
-              </footer>
-            </blockquote>
-          </Card.Body>
-          <Card.Footer className="text-muted">
-            <div className="d-grid gap-2">
-              <Button
-                size="lg"
-                onClick={handleChangeBgColor}
-                style={{ backgroundColor: bgColor }}
-              >
-                Generate New Quote
-              </Button>
-            </div>
-          </Card.Footer>
-        </Card>
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from "react";
+import { Card, Spinner } from "react-bootstrap";
+import { BsChatQuote } from "react-icons/bs";
+
+export default function Home() {
+  const [bgColor, setBgColor] = useState("#642b73");
+  const [quote, setQuote] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchQuote = () => {
+    setQuote(null);
+    setError(null);
+
+    const category = "happiness";
+    const apiKey = "Zkxq0JkjOF7CLixz3dLPrA==155oexel0hgd0wE5";
+
+    fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+      method: "GET",
+      headers: {
+        "X-Api-Key": apiKey,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setQuote(data[0]);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const colors = ["#642b73", "#c6426e", "#8C6495", "#983770", "#A33A70"];
+
+  const handleChangeBgColor = () => {
+    const currentColorIndex = colors.indexOf(bgColor);
+    const nextColorIndex = (currentColorIndex + 1) % colors.length;
+    setBgColor(colors[nextColorIndex]);
+
+    fetchQuote();
+  };
+  return (
+    <header className="App-header">
+      <Card
+        className="quote-card"
+        style={{ color: bgColor, border: "5px solid #642b73" }}
+      >
+        <Card.Header
+          className="quote-card-header"
+          style={{
+            color: bgColor,
+          }}
+        >
+          Quote For A Good Day
+        </Card.Header>
+        <Card.Body className="card-body" style={{ opacity: 0.9 }}>
+          <blockquote className="blockquote mb-0">
+            <br />
+            {error ? (
+              <span>Error: {error}</span>
+            ) : quote ? (
+              <>
+                <BsChatQuote size={70} />
+
+                <p className="quote-text">{quote.quote}</p>
+                <br />
+                <footer
+                  className="blockquote-footer"
+                  style={{ color: bgColor }}
+                >
+                  <strong>{quote.author} </strong>
+                  <cite title="Source Title">from API Ninjas</cite>
+                </footer>
+              </>
+            ) : (
+              <span>
+                <Spinner animation="grow" size="sm" />
+                <Spinner animation="grow" size="lg" />
+              </span>
+            )}
+          </blockquote>
+        </Card.Body>
+        <div className="center">
+          <button
+            className="generate-btn"
+            onClick={handleChangeBgColor}
+            style={{ backgroundColor: bgColor }}
+          >
+            <svg
+              width="300px"
+              height="60px"
+              viewBox="0 0 300 60"
+              className="border"
+            >
+              <polyline
+                points="299,1 299,59 1,59 1,1 299,1"
+                className="bg-line"
+              />
+              <polyline
+                points="299,1 299,59 1,59 1,1 299,1"
+                className="hl-line"
+              />
+            </svg>
+            <span>Generate New Quote</span>
+          </button>
+        </div>
+      </Card>
+    </header>
+  );
+}
